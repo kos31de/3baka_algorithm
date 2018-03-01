b3926390fadd09969abd531d8ed1b244fddb8766
kousy <32245877+kou-sy@users.noreply.github.com>
2018-03-01 12:11:37 +0900

    Update nabeatsu.php
    インデントを修正

--- nabeatsu.php
+++ nabeatsu.php
@@ -3,14 +3,14 @@
 for($i=1;$i<=1000;$i++){
 
     if($i%3 == 0 || strstr($i, '3') ) {
-        echo $i."dumb";
+       echo $i."dumb";
      }elseif($i%3 == 0){
        echo "idiot"
      }elseif (strstr($i, '3')) {
        echo "stupid"
      }
      else{
-        echo $i."<br/>";
+       echo $i."<br/>";
      }
        echo "\n";
 }
